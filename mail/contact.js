b2dbd40e01b39f25d43e585dab55b6c4e0484339
MarthaHellen <107993962+MarthaHellen@users.noreply.github.com>
2023-12-29 13:51:14 +0300

    OpenCommunityInitiative
    I mace changes to the Website and updated it to Open community Initiative.  U changed the email address. I did not change the social media

--- mail/contact.js
+++ mail/contact.js
@@ -15,7 +15,7 @@ $(function () {
             $this.prop("disabled", true);
 
             $.ajax({
-                url: "https://formsubmit.co/advocacyfordigitaldemocracy@gmail.com",
+                url: "https://formsubmit.co/opencommunityinitiative@gmail.com",
                 type: "POST",
                 data: {
                     name: name,
